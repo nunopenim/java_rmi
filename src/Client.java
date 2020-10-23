38c3085137805a5242153cb32ae6ccf7af98b797
nunopenim <nuno.penim@gmail.com>
2020-10-23 15:51:38 +0100

    general: added client class

---
+++ src/Client.java
@@ -0,0 +1,21 @@
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Client {
+
+    private Client() {}
+
+    public static void main(String[] args) {
+
+        String host = (args.length < 1) ? null : args[0];
+        try {
+            Registry registry = LocateRegistry.getRegistry(host);
+            Params stub = (Params) registry.lookup("RMI_Test");
+            String response = stub.pingMe();
+            System.out.println("response: " + response);
+        } catch (Exception e) {
+            System.err.println("Client exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+}
